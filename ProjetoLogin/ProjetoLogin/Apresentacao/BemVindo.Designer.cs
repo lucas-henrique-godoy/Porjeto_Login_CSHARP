098242b1995463aedcf889bd7ab2827d39c2acea
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-02-13 08:47:02 -0300

    Update BemVindo.Designer.cs

--- ProjetoLogin/ProjetoLogin/Apresentacao/BemVindo.Designer.cs
+++ ProjetoLogin/ProjetoLogin/Apresentacao/BemVindo.Designer.cs
@@ -28,19 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Mistral", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(149, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(305, 76);
+            label1.TabIndex = 0;
+            label1.Text = "Bem Vindo!!";
             // 
             // BemVindo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonHighlight;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(608, 328);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "BemVindo";
             Text = "BemVindo";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
